0fdf7130070f3b4057d11efc4cbd06929c6f9867
HooBBoo <wldbs272@naver.com>
2024-11-18 11:17:29 +0900

    [Update] 위치값 조정
    - TestMap 위치 0 0 0으로하고 테스트 중

--- Assets/Scripts/TriggerZone.cs
+++ Assets/Scripts/TriggerZone.cs
@@ -7,12 +7,14 @@ public class TriggerZone : MonoBehaviour
 {
     [Header("Teleport Position")]
     public Transform teleportPosition; // 텔레포트 할 위치
+    public float zOffset = 0f; // 보정이 필요하게 되면 조정
 
     // 플레이어 Teleport
     public void TeleportPlayer(Transform playerTransform)
     {
         // 트리거와 플레이어의 상대적인 위치 계산하여 이동(끊기는 느낌 안나게)
         Vector3 playerOffset = playerTransform.position - transform.position;
+        playerOffset.z += zOffset;
         playerTransform.position = teleportPosition.position + playerOffset;
     }
 
