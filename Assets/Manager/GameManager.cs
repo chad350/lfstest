3c516d76e9d9f78658ee2a656983053057a8f693
kjm0417 <ekdh41779@gmail.com>
2024-11-18 10:02:16 +0900

    Update : 매니저 제작

---
+++ Assets/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+       
+    }
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
